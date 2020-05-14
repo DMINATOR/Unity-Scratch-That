f871076672e7b25c51aea099de106b4985b3560f
Dmitri Kuznetsov <kna_int@mail.ru>
2020-05-14 20:46:24 +0300

    Initial commit

---
+++ Assets/Scripts/Game/Modes/GameModeBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameModeBase
+{
+    // Dictionary of game mode instances
+    private static Dictionary<string, GameModeBase> _gameModes = new Dictionary<string, GameModeBase>()
+    {
+        { nameof(PrototypeGameMode),  new PrototypeGameMode() }
+    };
+
+
+    public static GameModeBase GetModeByName(string name)
+    {
+        return _gameModes[name];
+    }
+}
---
+++ Assets/Scripts/Random/RandomTicketGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTicketGenerator
+{
+    int _seed;
+    int _position;
+
+    public RandomTicketGenerator(int seed)
+    {
+
+    }
+
+    public void TakeNextTicket()
+    {
+        _position++;
+    }
+
+    public void OffsetByTickets(int offset)
+    {
+        var cntr = 0;
+
+        while(cntr < offset)
+        {
+            TakeNextTicket();
+            cntr++;
+        }
+    }
+}
