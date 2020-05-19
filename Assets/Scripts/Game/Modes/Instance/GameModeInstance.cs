e56b940b578e558fb008aaeee6a41634ef2b9a22
Dmitri Kuznetsov <kna_int@mail.ru>
2020-05-19 22:31:16 +0300

    Further updates

--- Assets/Scripts/Game/Modes/Instance/GameModeInstance.cs
+++ Assets/Scripts/Game/Modes/Instance/GameModeInstance.cs
@@ -2,8 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(GameModeInstanceLocator))]
 public class GameModeInstance : MonoBehaviour
 {
+
+    [Header("Locator")]
+
+    [Tooltip("Locator")]
+    public GameModeInstanceLocator Locator;
+
+    [Header("Variables")]
+
+    [Tooltip("Seed used for random generation")]
+    public int Seed;
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/Game/Modes/Instance/GameModeInstanceLocator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameModeInstanceLocator : MonoBehaviour
+{
+    [Tooltip("Game mode")]
+    public GameModeDefinitionBase GameMode;
+
+    [Tooltip("Statistics")]
+    public GameModeInstanceStatistics Statistics;
+}
